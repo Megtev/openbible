367d94bfa659ff35dd7f17b041bb794de85e5887
Megtev <illiasmoliarchuk@icloud.com>
2020-09-23 18:04:33 +0300

    make small changes: add TODO.txt list to understand how many things to do, and add grid layout

--- openbible/openbible.py
+++ openbible/openbible.py
@@ -7,42 +7,63 @@ class OpenBibleMainGui(QtWidgets.QWidget):
 
     def __init__(self):
         super().__init__()
-        self.show_button = QtWidgets.QPushButton('Show', self)
-        self.hide_button = QtWidgets.QPushButton('Hide', self)
-        self.just_button = QtWidgets.QPushButton('Just button', self)
-        self.init_ui()
-        self.show_info = ShowInfo()
 
-    def init_ui(self):
-        # init main UI
-        self.setGeometry(300, 300, 300, 220)
+        # Set basic geometry of the main window
+        self.resize(600, 400)
         self.setWindowTitle('OpenBible')
 
-        self.show_button.move(50, 50)
-        self.show_button.clicked.connect(self.show_slides)
+        # Add menu bar
+        # TODO implement all standart features
+        self.menubar = QtWidgets.QMenuBar(self)
+        self.menubar.addMenu('File')
 
-        self.hide_button.move(50, 75)
-        self.hide_button.clicked.connect(self.hide_slides)
+        # Add grid layout
+        main_grid = QtWidgets.QGridLayout()
+        main_grid.setHorizontalSpacing(12)
+        main_grid.setVerticalSpacing(24)
+        self.setLayout(main_grid)
 
-        self.just_button.move(50, 100)
-        self.just_button.hide()
-        # self.show_button.setText('Hide')
+        # Add second window to show info
+        # TODO driver to show and control all info in second window
+        self.show_info = ShowInfo()
 
-        self.show()
+        # Add some test buttons -----------------
+        show_button = QtWidgets.QPushButton('Show', self)
+        hide_button = QtWidgets.QPushButton('Hide', self)
+        just_button = QtWidgets.QPushButton('Just button', self)
+
+        # self.show_button.move(50, 50)
+        show_button.clicked.connect(self.show_slides)
+        hide_button.clicked.connect(self.hide_slides)
+
+        main_grid.addWidget(show_button, 1, 1)
+        main_grid.addWidget(hide_button, 2, 1)
+        # main_grid.add
+        # hide_button.hide()
+        just_button.hide()
+        # End of test button code ---------------
+
+        # Move main window to the center of display and show the window
+        self.move_to_center()
 
     def show_slides(self):
         # Create new windows that shows text on second screen
         # TODO
         print('Show')
         self.show_info.show()
-        self.just_button.show()
-        # self.show_button.setText('Hide')
+        # self.just_button.show()
 
     def hide_slides(self):
         # Hide additional window
         print('Hide')
         self.show_info.hide()
-        self.just_button.hide()
+        # self.just_button.hide()
+
+    def move_to_center(self):
+        qr = self.frameGeometry()
+        cp = QtWidgets.QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 
     # Build-in functions
 
@@ -55,6 +76,7 @@ class OpenBibleMainGui(QtWidgets.QWidget):
             QtWidgets.QMessageBox.No)
 
         if reply == QtWidgets.QMessageBox.Yes:
+            self.show_info.close()
             event.accept()
         else:
             event.ignore()
@@ -78,4 +100,5 @@ class ShowInfo(QtWidgets.QWidget):
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     main_window = OpenBibleMainGui()
+    main_window.show()
     sys.exit(app.exec_())
